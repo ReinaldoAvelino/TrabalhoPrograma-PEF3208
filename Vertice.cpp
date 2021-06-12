a3280d586a724865d4544dbbcd9c7919267a96ab
Reinaldo Avelino <73717173+ReinaldoAvelino@users.noreply.github.com>
2021-06-12 00:09:22 -0300

    Update Vertice.cpp

--- Vertice.cpp
+++ Vertice.cpp
@@ -12,13 +12,6 @@ char Vertice::getNome() {
     return this->nome;
 }
 
-bool Vertice::conecta(Vertice* destino) {
-    if (this->quantidade == MAXIMO_ARESTAS || destino->quantidade == MAXIMO_ARESTAS) return false;
-    this->adjacentes[this->quantidade++] = destino;
-    destino->adjacentes[destino->quantidade++] = this;
-    return true;
-}
-
 int Vertice::getNumeroDeArestrasDeSaida() {
     return quantidade;
 }
