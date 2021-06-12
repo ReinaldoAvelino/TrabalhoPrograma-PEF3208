cd62afa66d532283d72638dd7b74442e3a08909f
Reinaldo Avelino <73717173+ReinaldoAvelino@users.noreply.github.com>
2021-06-12 00:08:50 -0300

    Update Grafo.h

--- Grafo.h
+++ Grafo.h
@@ -1,21 +1,28 @@
 #ifndef GRAFO_H
 #define GRAFO_H
 
-#define MAXIMO_VERTICES 30
+#define MAXIMO_VERTICES 20
+#define MAXIMO_ARESTAS 20
 
 #include "Vertice.h"
+#include "Aresta.h"
 
 class Grafo {
 public:
 	Grafo();
 	virtual ~Grafo();
-	bool adicionar(char nome);
+	bool adicionarVertice(char nome);
+	bool adicionarAresta(char* nome, float comprimento, float inclinacao);
 	Vertice* get(char nome);
 	int getNumeroVertices();
 	Vertice** getVertices();
+	int getNumeroArestas();
+	Aresta** getArestas();
 protected:
     int numeroVertices = 0;
     Vertice* vertices[MAXIMO_VERTICES];
+    int numeroArestas = 0;
+    Aresta* arestas[MAXIMO_ARESTAS];
 };
 
 #endif // GRAFO_H
