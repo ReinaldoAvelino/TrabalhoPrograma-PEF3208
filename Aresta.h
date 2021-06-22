364197b2bc490926cb3302140a7685859ac4e51c
Reinaldo Avelino <73717173+ReinaldoAvelino@users.noreply.github.com>
2021-06-22 20:24:16 -0300

    Update Aresta.h

--- Aresta.h
+++ Aresta.h
@@ -7,11 +7,16 @@ class Aresta {
 public:
     Aresta(char* nome, float comprimento, float inclinacao);
     virtual ~Aresta();
+    char* getNome();
+    void addDistribuicao(double intensidade, int forma); // adiciona uma carga de forca distribuida
+                                                         // forma 1 para retangulo e 2 para triangulo
 
 protected:
     char* nome;
     float comprimento;
-    float inclinacao;
+    float inclinacao; // habilitado somente para 0 ou 90 graus
+    double intensidade; // intensidade da forca distribuida
+    int forma; // forma geometrica da forca distribuida
 };
 
 #endif // ARESTA_H
