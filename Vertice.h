a63a601d01534868c9c9be5b3c36ee9f4f7cba3d
Reinaldo Avelino <73717173+ReinaldoAvelino@users.noreply.github.com>
2021-06-22 20:25:58 -0300

    Update Vertice.h

--- Vertice.h
+++ Vertice.h
@@ -3,16 +3,21 @@
 
 using namespace std;
 
+#define MAXIMO_ARESTAS 4 // maximo de arestas por vertice
+#define MAXIMO_FORCAS 2 // maximo de forcas por vertice
+
 class Vertice {
 public:
 	Vertice(char nome);
 	virtual ~Vertice();
 	char getNome();
 	int getNumeroDeArestrasDeSaida();
+	bool addForca(double intensidade, char direcao); // direcao pode ser 'i' ou 'j'
 protected:
 	char nome;
-	Vertice* adjacentes[4];
+	Vertice* adjacentes[MAXIMO_ARESTAS];
 	int quantidade = 0;
+	double forcas[MAXIMO_FORCAS];
 };
 
 #endif // VERTICE_H
